c54a6fb9fc8aace197572c55a267c1d039c78750
Altofy <45941854+Alotfy55@users.noreply.github.com>
2019-03-19 19:33:20 +0200

    latest build

--- Kojack player/Kojack player/Source.cpp
+++ Kojack player/Kojack player/Source.cpp
@@ -1,24 +1,42 @@
-// let the magic begin 
-#include <SFML/Graphics.hpp>
-
-int main()
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+void playMusic(const std::string& filename)
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	std::cout << filename <<std::endl;
+	// Load an ogg music file
+	sf::Music music;
+	if (!music.openFromFile("resources/" + filename + ".wav"))
+		return;
 
-	while (window.isOpen())
+	// Display music informations
+	std::cout << filename << ":" << std::endl;
+	std::cout << " " << music.getDuration().asSeconds() << " seconds" << std::endl;
+	std::cout << " " << music.getSampleRate() << " samples / sec" << std::endl;
+	std::cout << " " << music.getChannelCount() << " channels" << std::endl;
+
+	// Play it
+	music.play();
+
+	// Loop while the music is playing
+	while (music.getStatus() == sf::Music::Playing)
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+		// Leave some CPU time for other processes
+		sf::sleep(sf::milliseconds(100));
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+		// Display the playing position
+		std::cout << "\rPlaying... " << music.getPlayingOffset().asSeconds() << " sec        ";
+		std::cout << std::flush;
 	}
-	return 0;
+	std::cout << std::endl << std::endl;
+}
+int main () 
+{
+	std::string x;
+	getline(std::cin, x);
+	playMusic(x);
+	return main (); 
 }
