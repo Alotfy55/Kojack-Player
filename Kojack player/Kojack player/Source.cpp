f429d120ce82d28a20b85996d88d7a035efad049
Alotfy55 <45941854+Alotfy55@users.noreply.github.com>
2019-03-21 00:10:56 +0200

    test

--- Kojack player/Kojack player/Source.cpp
+++ Kojack player/Kojack player/Source.cpp
@@ -35,8 +35,9 @@ struct song
 	string artist;
 	int rating;
 };
+vector <song> songs_data; 
 //accessing the data of each song with a unique id , another idea is fuck ids we use the songs name to call for its data
-int song_entrey();
+
 map <int, song> sang;
 void playMusic(const string& filename)
 {
@@ -52,18 +53,26 @@ void playMusic(const string& filename)
 	cout << " " << music.getSampleRate() << " samples / sec" << endl;
 	cout << " " << music.getChannelCount() << " channels" << endl;
 
+		char x;
 	// Play it
 	music.play();
 
 	// Loop while the music is playing
-	while (music.getStatus() == sf::Music::Playing)
+	cout << "press p to pause"; 
+
+	while (music.getStatus() == sf::Music::Playing || music.getPlayingOffset().asSeconds() < music.getDuration().asSeconds())
 	{
 		// Leave some CPU time for other processes
 		sf::sleep(sf::milliseconds(100));
-
 		// Display the playing position
 		cout << "\rPlaying... " << music.getPlayingOffset().asSeconds() << " sec       ";
 		cout << flush;
+		if (cin >> x, 'p')
+			music.pause();
+		if (cin >> x, 'p')
+			music.play();
+		if (cin >> x, 'n')
+			break; 
 	}
 	cout << endl << endl;
 }
@@ -73,7 +82,7 @@ int main ()
 	getline(cin, directory);
 	
 	get_all_files_names_within_folder(directory);
-	
+
 	for (int i = 0; i <= songs.size() ; i++)
 	{
 		playMusic(songs[i]);
@@ -81,3 +90,4 @@ int main ()
 	system("pause");
 	return 0; 
 }
+//test for sth
