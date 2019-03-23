6c9b524c9f1b65113742c805a8c4c64e2cbb9655
Alotfy55 <45941854+Alotfy55@users.noreply.github.com>
2019-03-24 00:17:08 +0200

    Update Source.cpp

--- Kojack player/Kojack player/Source.cpp
+++ Kojack player/Kojack player/Source.cpp
@@ -25,8 +25,6 @@ void get_all_files_names_within_folder(string folder)
 		::FindClose(hFind);
 	}
 }
-
-
 struct song
 {
 	string name;
@@ -35,11 +33,13 @@ struct song
 	string artist;
 	int rating;
 };
+
+
 vector <song> songs_data; 
 //accessing the data of each song with a unique id , another idea is fuck ids we use the songs name to call for its data
 
 map <int, song> sang;
-void playMusic(const string& filename)
+void playMusic(const string& filename ,  int play_num)
 {
 	cout << filename << endl;
 	// Load an ogg music file
@@ -59,7 +59,7 @@ void playMusic(const string& filename)
 
 	// Loop while the music is playing
 
-	cout << "Press p to pause , press it again to play \n Press n to play next song  \n Press s to stop music ";
+	cout << "Press p to pause , press it again to play \n Press n to play next song  \n Press s to stop music \n Press b to go back \n";
 	while (music.getStatus() == sf::Music::Playing || music.getPlayingOffset().asSeconds() < music.getDuration().asSeconds())
 	{
 		// Leave some CPU time for other processes
@@ -73,11 +73,26 @@ void playMusic(const string& filename)
 				music.pause();
 			else if ( x == 'p' )
 				music.play();
-			else if ( x == 'n')
-				break;
-			else if ( x == 's')
+			else if (x == 'n')
 			{
 				music.stop();
+				playMusic(songs[play_num+1],play_num+1);
+				return;
+			}
+			else if ( x == 's' && music.getStatus() != sf::Music::Playing )
+			{
+				break ;
+			}
+			else if (x == 's' && music.getStatus() == sf::Music::Playing)
+			{
+				music.stop();
+			}
+			else if (x == 'b')
+			{
+				//b as in backwards 
+				music.stop();
+				playMusic(songs[play_num - 1], play_num - 1);
+
 			}
 		}
 	}
@@ -92,11 +107,18 @@ int main ()
 	
 	get_all_files_names_within_folder(directory);
 
-	for (int i = 0; i <= songs.size() ; i++)
+	cout << "Please enter the number of the song you want to play from the following list : " << endl;
+	for (int i = 1; i <= songs.size() ; i++)
 	{
-		playMusic(songs[i]);
-		if (i == songs.size()-1)
-			i = -1;
+		cout << i << " : " << songs[i-1] << endl ; 
+	}
+	while (true)
+	{
+		int x;
+		if (cin >> x)
+		{
+			playMusic(songs[x-1] , (x-1) );
+		}
 	}
 	system("pause");
 	return 0; 
